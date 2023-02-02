1f1b1caa883189b055f409564ac0505d24485d6c
DenisSherbina <denisikus17@mail.ru>
2023-02-02 04:12:19 +0300

    Add files via upload

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
 import { HttpService } from '../services/http.service';
 import { pogoda } from './../model/pogoda'
 
@@ -10,38 +9,38 @@ import { pogoda } from './../model/pogoda'
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private router: Router, private http: HttpService) { }
+  constructor(private http: HttpService) { }
 
-  public sity: Array<string> = ['Москва', 'Ереван', 'Оренбург', 'Ростов-на-Дону'] // сюда загружаем массив объектов (список брендов)
+  public sity: Array<string> = [] // сюда загружаем массив объектов (список брендов)
 
-  public pogoda: pogoda
+  public pogoda: pogoda // сюда подгрузим информацию из конкретного города
 
   public confirm: boolean = false // кнопка подтверждения
 
-  public actual_date: Date | null = new Date()
+  public actual_date: Date | null = new Date() // отобразим сегодняшнюю дату
 
   ngOnInit(): void {
-    console.log('HomeComponent INIT');
+    // инициализация проекта
     this.start_page()
   }
 
-  start_page() {
+  async start_page() {
     // Загружаем список городов
     this.confirm = false
     this.sity = []
     this.pogoda = undefined
+    await new Promise( (resolve) => { setTimeout(() => { console.log('Специально небольшая задержка в 500 мс'); return resolve(1); }, 500) })
     this.http.get('getAllSity')
       .subscribe(
         res => {
           console.log('\nОтвет сервер: ', res)
-          this.confirm = true
+          this.confirm = true // вернём возможность кнопке обновления... обновлять :)
           // запишем наши города
           this.sity = res
-          console.log('this.sity: ', this.sity)
         })
   }
 
-  async Loading_pogoda(event) {
+  Loading_pogoda(event) {
     let get_sity = this.getSelect().value
     console.log('Загружаю контент: ', get_sity)
     // Загружаем погоду в городе
